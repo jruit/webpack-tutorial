baad10f5040594eca742f47e6b842d6903d10659
jruit <929322398@qq.com>
2020-08-15 16:12:54 +0800

    babel

---
+++ 3/webpack3-3/a.js
@@ -0,0 +1,2 @@
+let add = (a, b) => a + b;
+console.log(add(3, 5));
---
+++ 3/webpack3-3/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './a.js',
+  output: {
+    path: path.resolve(__dirname, ''),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  mode: 'none'
+};
