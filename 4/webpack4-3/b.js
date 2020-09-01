20eb03f011bdbc5378bee83f1870b0b60df9d8e0
jruit <929322398@qq.com>
2020-09-01 21:56:29 +0800

    4-3

---
+++ 4/webpack4-3/b.js
@@ -0,0 +1,1 @@
+export var name = 'Rose9';
---
+++ 4/webpack4-3/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  entry: './a.js',
+  output: {
+    path: path.resolve(__dirname, ''),
+    filename: 'bundle.js'
+  },
+  devServer: {
+      liveReload: false
+  },
+  mode: 'none'
+};
